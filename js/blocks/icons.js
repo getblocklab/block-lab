d4e4d5f1f98b4888c3c26f9bc5bc3ef6a4cca089
Rheinard Korf <rheinard@thekorfs.com>
2018-07-30 11:53:16 +1000

    Update logo

--- js/blocks/icons.js
+++ js/blocks/icons.js
@@ -1,11 +1,17 @@
 export default {
 	logo: (
-		<svg id='logo' xmlns='http://www.w3.org/2000/svg' width='20' height='20'
-		viewBox='0 0 20 20'>
-		<circle shapeRendering='geometricPrecision' cx='8' cy='8' r='7' stroke='#fff'
-		strokeWidth='1.3' fill='transparent' />
-		<circle shapeRendering='geometricPrecision' cx='8' cy='8' r='3' stroke='#fff'
-		strokeWidth='0.7' fill='#fff' />
+		<svg viewBox='0 0 98 85' xmlns='http://www.w3.org/2000/svg' fillRule='evenodd'
+		     clipRule='evenodd' strokeLinecap='round' strokeLinejoin='round' strokeMiterlimit='1.5'>
+			<g id='ACB_Icon_Primary' strokeWidth='3'>
+				<path d='M17.777,29.006l15.566,-26.962l31.133,0l-15.567,26.962l-31.132,0Z'
+				      fill='none' stroke='#180000' />
+				<path d='M17.777,82.929l15.566,-26.962l31.133,0l-15.567,26.962l-31.132,0Z'
+				      fill='none' stroke='#180000' />
+				<path d='M64.476,55.967l-31.133,-53.923l31.133,0l31.132,53.923l-31.132,0Z'
+				      fill='none' stroke='#180000' />
+				<path d='M95.608,55.967l-15.828,26.508l-30.871,0.454l-15.566,-26.962l-15.566,26.962l-15.566,-26.962l15.566,-26.961l15.566,-26.962l15.566,26.962l15.567,26.961l31.132,0Z'
+				      stroke='#000' />
+			</g>
 		</svg>
 	)
 }
