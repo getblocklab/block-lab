ee29ab589c526fc981db3180338c5a02d3a6c67e
Rheinard Korf <rheinard@thekorfs.com>
2018-08-03 12:20:44 +1000

    Basic bundling with 'gulp'.
    - @todo: Add i18n build tasks.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require( 'gulp' );
+var del = require('del');
+var run = require( 'gulp-run' );
+
+gulp.task( 'run:build', function() {
+	return run('npm run build').exec();
+})
+
+gulp.task( 'bundle', function () {
+	return gulp.src( [
+		'**/*',
+		'!node_modules/**/*',
+		'!js/blocks/**/*',
+		'!js/src/**/*',
+		'!js/tests/**/*',
+		'!js/coverage/**/*',
+		'!package/**/*',
+	] )
+		.pipe( gulp.dest( 'package' ) );
+} );
+
+gulp.task('remove:bundle', function () {
+	return del([
+		'package',
+	]);
+});
+
+gulp.task('clean:bundle', function () {
+	return del([
+		'package/node_modules',
+		'package/js/blocks',
+		'package/js/src',
+		'package/tests',
+		'package/coverage',
+		'package/package',
+		'package/gulpfile.js',
+		'package/Makefile',
+		'package/package*.json',
+		'package/phpunit.xml.dist',
+		'package/webpack.config.js',
+	]);
+});
+
+gulp.task('default', gulp.series('remove:bundle','run:build','bundle','clean:bundle'));
