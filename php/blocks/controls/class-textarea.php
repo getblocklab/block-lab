0cb07e0da8611128c454ff8e717319848bf3a32b
Luke Carbis <luke@carb.is>
2018-08-11 09:20:58 +1000

    Add sanitisation changes to textarea

--- php/blocks/controls/class-textarea.php
+++ php/blocks/controls/class-textarea.php
@@ -76,6 +76,9 @@ class Textarea extends Control_Abstract {
 			'type'    => 'number',
 			'default' => '',
 			'sanitize' => function( $value ) {
+				if ( empty( $value ) || '0' === $value ) {
+					return null;
+				}
 				return (int) filter_var( $value, FILTER_SANITIZE_NUMBER_INT );
 			}
 		) );
