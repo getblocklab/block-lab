9aa3639be934a5905e87116bf74889cc81c7a974
Luke Carbis <luke.carbis@xwp.co>
2018-08-30 12:06:53 +1000

    PHPCS fixes.

--- php/autoloader.php
+++ php/autoloader.php
@@ -10,43 +10,44 @@
 /**
  * Register Autoloader
  */
-spl_autoload_register( function ( $class ) {
+spl_autoload_register(
+	function ( $class ) {
+		// Assume we're using namespaces (because that's how the plugin is structured).
+		$namespace = explode( '\\', $class );
+		$root      = array_shift( $namespace );
 
-	// Assume we're using namespaces (because that's how the plugin is structured).
-	$namespace = explode( '\\', $class );
-	$root      = array_shift( $namespace );
+		// If a class ends with "Trait" then prefix the filename with 'trait-', else use 'class-'.
+		$class_trait = preg_match( '/Trait$/', $class ) ? 'trait-' : 'class-';
 
-	// If a class ends with "Trait" then prefix the filename with 'trait-', else use 'class-'.
-	$class_trait = preg_match( '/Trait$/', $class ) ? 'trait-' : 'class-';
+			// If we're not in the plugin's namespace then just return.
+		if ( 'Advanced_Custom_Blocks' !== $root ) {
+			return;
+		}
 
-	// If we're not in the plugin's namespace then just return.
-	if ( 'Advanced_Custom_Blocks' !== $root ) {
-		return;
+		// Class name is the last part of the FQN.
+		$class_name = array_pop( $namespace );
+
+			// Remove "Trait" from the class name.
+		if ( 'trait-' === $class_trait ) {
+			$class_name = str_replace( 'Trait', '', $class_name );
+		}
+
+		// For file naming, the namespace is everything but the class name and the root namespace.
+		$namespace = trim( implode( DIRECTORY_SEPARATOR, $namespace ) );
+
+		// Get the path to our files.
+		$directory = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . '../php';
+		if ( ! empty( $namespace ) ) {
+			$directory .= DIRECTORY_SEPARATOR . $namespace;
+		}
+
+		$file = $directory . DIRECTORY_SEPARATOR . $class_trait . $class_name . '.php';
+
+		// Because WordPress file naming conventions are odd.
+		$file = strtolower( str_replace( '_', '-', $file ) );
+
+		if ( file_exists( $file ) ) {
+			require_once $file;
+		}
 	}
-
-	// Class name is the last part of the FQN.
-	$class_name = array_pop( $namespace );
-
-	// Remove "Trait" from the class name.
-	if ( 'trait-' === $class_trait ) {
-		$class_name = str_replace( 'Trait', '', $class_name );
-	}
-
-	// For file naming, the namespace is everything but the class name and the root namespace.
-	$namespace = trim( implode( DIRECTORY_SEPARATOR, $namespace ) );
-
-	// Get the path to our files.
-	$directory = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . '../php';
-	if ( ! empty( $namespace ) ) {
-		$directory .= DIRECTORY_SEPARATOR . $namespace;
-	}
-
-	$file = $directory . DIRECTORY_SEPARATOR . $class_trait . $class_name . '.php';
-
-	// Because WordPress file naming conventions are odd.
-	$file = strtolower( str_replace( '_', '-', $file ) );
-
-	if ( file_exists( $file ) ) {
-		require_once( $file );
-	}
-} );
+);
