cb717679119b73d3f223c7c70a60a405e70aa8e4
Justin Kopepasah <justin@kopepasah.com>
2018-12-05 19:05:51 +0900

    Add an error class name to the TextControl inputs.

--- js/blocks/controls/number.js
+++ js/blocks/controls/number.js
@@ -1,8 +1,24 @@
+import classNames from 'classnames';
+
 const { TextControl } = wp.components;
 
 const BlockLabNumberControl = ( props, field, block ) => {
 	const { setAttributes } = props;
 	const attr = { ...props.attributes };
+
+	/**
+	 * Sets the Error Class for the Text Control.
+	 *
+	 * @param {string} element The HTML element.
+	 *
+	 * @return {void}
+	 */
+	const setErrorClass = ( element, valid ) => {
+		element.className = classNames( 'components-text-control__input', {
+			'text-control__error': valid
+		} );
+	}
+
 	return (
 		<TextControl
 			type="number"
@@ -16,13 +32,11 @@ const BlockLabNumberControl = ( props, field, block ) => {
 				setAttributes( attr )
 			}}
 			onFocus={ event => {
+				setErrorClass( document.activeElement, false );
 				event.target.reportValidity()
 			}}
 			onBlur={ event => {
-				if ( ! event.target.checkValidity() ) {
-					let input = Object.assign({},event).target
-					setTimeout(() => input.focus(), 10);
-				}
+				setErrorClass( event.target, ! event.target.checkValidity() );
 			}}
 		/>
 	)
