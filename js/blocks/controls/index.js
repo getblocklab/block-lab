35c91389328533f6fef21f8459cdee71c0249bac
Justin Kopepasah <justin@kopepasah.com>
2018-12-12 16:26:59 +0900

    Wrap all controls in quotes.

--- js/blocks/controls/index.js
+++ js/blocks/controls/index.js
@@ -12,16 +12,16 @@ import BlockLabToggleControl from './toggle'
 import BlockLabColorPickerControl from './color-picker'
 
 export default {
-	text: BlockLabTextControl,
-	textarea: BlockLabTextareaControl,
-	url: BlockLabURLControl,
-	email: BlockLabEmailControl,
-	number: BlockLabNumberControl,
-	checkbox: BlockLabCheckboxControl,
-	radio: BlockLabRadioControl,
-	range: BlockLabRangeControl,
-	select: BlockLabSelectControl,
-	multiselect: BlockLabMultiselectControl,
-	toggle: BlockLabToggleControl,
+	'text': BlockLabTextControl,
+	'textarea': BlockLabTextareaControl,
+	'url': BlockLabURLControl,
+	'email': BlockLabEmailControl,
+	'number': BlockLabNumberControl,
+	'checkbox': BlockLabCheckboxControl,
+	'radio': BlockLabRadioControl,
+	'range': BlockLabRangeControl,
+	'select': BlockLabSelectControl,
+	'multiselect': BlockLabMultiselectControl,
+	'toggle': BlockLabToggleControl,
 	'color-picker': BlockLabColorPickerControl,
 }
