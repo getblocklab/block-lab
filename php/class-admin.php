6ffcfd298f0d112fb9a1a63bf8a31aa1367b6e51
Luke Carbis <luke.carbis@xwp.co>
2019-01-30 12:36:29 +1000

    Add Go Pro menu item

--- php/class-admin.php
+++ php/class-admin.php
@@ -34,5 +34,48 @@ class Admin extends Component_Abstract {
 	 * Register any hooks that this component needs.
 	 */
 	public function register_hooks() {
+		if ( $this->show_pro ) {
+			add_action( 'admin_menu', array( $this, 'add_submenu_pages' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		}
+	}
+
+	/**
+	 * Enqueue scripts and styles used globally in the WP Admin.
+	 *
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_style(
+			'block-lab',
+			$this->plugin->get_url( 'css/admin.css' ),
+			array(),
+			$this->plugin->get_version()
+		);
+	}
+
+	/**
+	 * Add submenu pages to the Block Lab menu.
+	 */
+	public function add_submenu_pages() {
+		add_submenu_page(
+			'edit.php?post_type=block_lab',
+			__( 'Block Lab Pro', 'block_lab' ),
+			__( 'Go Pro', 'block_lab' ),
+			'manage_options',
+			'block-lab-pro',
+			array( $this, 'render_pro_page' )
+		);
+	}
+
+	/**
+	 * Render the Pro upgrade page.
+	 */
+	public function render_pro_page() {
+		?>
+		<div class="wrap block-lab-pro">
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+		</div>
+		<?php
 	}
 }
