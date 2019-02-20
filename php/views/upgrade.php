a7685193d1abae9d8962f9447478604752fe574e
Rob Stinson <r.stinson11@gmail.com>
2019-02-20 10:15:40 +1000

    Initital template, styling, and content for the go pro page

--- php/views/upgrade.php
+++ php/views/upgrade.php
@@ -8,3 +8,102 @@
  */
 
 ?>
+<div class="bl_admin">
+    <section class="bl_container">
+        <div class="bl_notification_bar bl_tile">
+            <div class="bl_tile_body">
+                <span>This is a notification message.</span>
+                <button class="bl_button_close">×</button>
+            </div>
+        </div>
+        <div class="bl_dashboard_welcome bl_tile" style="background-image: url('<?php echo esc_attr( block_lab()->get_assets_url( 'images/Block-Lab-Pro-Hero-Background-1.svg' ) ); ?>');">
+            <div class="bl_tile_body">
+                <span>Block Lab Pro</span>
+                <h1>Early Access Pass</h1>
+                <p>A custom block building experience perfect for agencies and freelancers.</p>
+                <a class="bl_button bl_button--white" href="">Check out Early Access</a>
+                <a class="bl_button bl_button--white-stroke" href="">Enter Your License Key</a>
+            </div>
+        </div>
+        <!-- Dashboard Tile -->
+        <div class="bl_tile bl_tile_2">
+            <div class="bl_tile_header">
+                <span>Pro Fields</span>
+            </div>
+            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
+            <div class="bl_tile_body">
+                <ul class="bl_ul_pills">
+                    <li>Repeater Field</li>
+                    <li>Post Object Field</li>
+                    <li>oEmbed Field</li>
+                    <li>Color Palette Field</li>
+                    <li>Date Field</li>
+                    <li>Time Field</li>
+                    <li>Map Field</li>
+                    <li>Code Editor Field</li>
+                    <li>User Object Field</li>
+                    <li>Menu Field</li>
+                    <li>Field Upload Field</li>
+                    <li>Gallery Field</li>
+                    <li>Video Field</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Dashboard Tile -->
+        <div class="bl_tile bl_tile_2">
+            <div class="bl_tile_header">
+                <span>Pro Features</span>
+            </div>
+            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
+            <div class="bl_tile_body">
+                <ul class="bl_ul_pills">
+                    <li>Block Alignment Controls</li>
+                    <li>Block Sizing Controls</li>
+                    <li>Field Conditions</li>
+                    <li>Block Conditions</li>
+                    <li>Validation Rules</li>
+                    <li>White Labelling</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Dashboard Tile -->
+        <div class="bl_tile bl_tile_2">
+            <div class="bl_tile_header">
+                <span>Support</span>
+            </div>
+            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
+            <div class="bl_tile_body">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Dashboard Tile -->
+        <div class="bl_tile bl_tile_3">
+            <div class="bl_tile_header">
+                <span>★★ Loving Block Lab? ★★</span>
+            </div>
+            <div class="bl_tile_body">
+                <p>If Block Lab has helped you make the most of Gutenberg and custom blocks for your site, leave us a review on WordPress.org.</p>
+            </div>
+            <div class="bl_tile_footer">
+                <a class="bl_button" href="">★ Leave Review</a>
+            </div>
+        </div>
+        <!-- Dashboard Tile -->
+        <div class="bl_tile bl_tile_3">
+            <div class="bl_tile_header">
+                <span>Get more out of Block Lab</span>
+            </div>
+            <div class="bl_tile_body">
+                <p>Subscribe to our newsletter for news, updates, and tutorials on working with Gutenberg.</p>
+            </div>
+            <div class="bl_tile_footer">
+                <a class="bl_button" href="">Subscribe</a>
+            </div>
+        </div>
+        <div class="bl_section_heading">
+            <h2>Block Lab Pro Features</h2>
+        </div>
+    </section>
+</div>
