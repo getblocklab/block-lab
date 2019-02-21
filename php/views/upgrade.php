bb5f005622ef4ee259bb0564ad4b6bc2860fca37
Rob Stinson <r.stinson11@gmail.com>
2019-02-21 14:43:05 +1000

    assets, styling, and content for the go pro page

--- php/views/upgrade.php
+++ php/views/upgrade.php
@@ -10,96 +10,68 @@
 ?>
 <div class="bl_admin">
     <section class="bl_container">
-        <div class="bl_notification_bar bl_tile">
-            <div class="bl_tile_body">
-                <span>This is a notification message.</span>
-                <button class="bl_button_close">×</button>
-            </div>
-        </div>
         <div class="bl_dashboard_welcome bl_tile" style="background-image: url('<?php echo esc_attr( block_lab()->get_assets_url( 'images/Block-Lab-Pro-Hero-Background-1.svg' ) ); ?>');">
             <div class="bl_tile_body">
-                <span>Block Lab Pro</span>
-                <h1>Early Access Pass</h1>
-                <p>A custom block building experience perfect for agencies and freelancers.</p>
-                <a class="bl_button bl_button--white" href="">Check out Early Access</a>
-                <a class="bl_button bl_button--white-stroke" href="">Enter Your License Key</a>
+                <div>
+                    <span>Block Lab Pro</span>
+                    <h1>Early Access Pass</h1>
+                    <p>Lock in a lifetime discount and help kickstart Block Lab Pro.</p>
+                    <a target="_blank" class="bl_button bl_button--white" href="https://getblocklab.com/pro">Check out Early Access</a>
+                    <a class="bl_button bl_button--white-stroke" href="">Enter Your License Key</a>
+                </div>
+                <div>
+                    <img class="bl_early_access_pass" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_early_access_pass.svg' ) ); ?>" alt="">
+                </div>
             </div>
         </div>
         <!-- Dashboard Tile -->
         <div class="bl_tile bl_tile_2">
             <div class="bl_tile_header">
-                <span>Pro Fields</span>
+                <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_newfields.svg' ) ); ?>" alt="">
             </div>
-            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
-            <div class="bl_tile_body">
-                <ul class="bl_ul_pills">
-                    <li>Repeater Field</li>
-                    <li>Post Object Field</li>
-                    <li>oEmbed Field</li>
-                    <li>Color Palette Field</li>
-                    <li>Date Field</li>
-                    <li>Time Field</li>
-                    <li>Map Field</li>
-                    <li>Code Editor Field</li>
-                    <li>User Object Field</li>
-                    <li>Menu Field</li>
-                    <li>Field Upload Field</li>
-                    <li>Gallery Field</li>
-                    <li>Video Field</li>
-                </ul>
+            <div class="bl_tile_body"> 
+                <h4 class="bl_align_center">New Fields</h4>
+                <p class="bl_align_center">More fields including repeater, post object, and more to help you build the custom blocks you need for yourself and your clients.</p>
             </div>
         </div>
         <!-- Dashboard Tile -->
         <div class="bl_tile bl_tile_2">
             <div class="bl_tile_header">
-                <span>Pro Features</span>
+                <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_newfields.svg' ) ); ?>" alt="">
             </div>
-            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
             <div class="bl_tile_body">
-                <ul class="bl_ul_pills">
-                    <li>Block Alignment Controls</li>
-                    <li>Block Sizing Controls</li>
-                    <li>Field Conditions</li>
-                    <li>Block Conditions</li>
-                    <li>Validation Rules</li>
-                    <li>White Labelling</li>
-                </ul>
+                <h4 class="bl_align_center">New Features</h4>
+                <p class="bl_align_center">Features including conditional logic, custom validation, and white-labeling, to help you extend Block Lab and leverage the best of Gutenberg.</p>
             </div>
         </div>
         <!-- Dashboard Tile -->
         <div class="bl_tile bl_tile_2">
             <div class="bl_tile_header">
-                <span>Support</span>
+                <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_newfields.svg' ) ); ?>" alt="">
             </div>
-            <img src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wp_admin_image_1.svg' ) ); ?>" alt="">
             <div class="bl_tile_body">
-                <ul>
-                    <li></li>
-                </ul>
+                <h4 class="bl_align_center">Support & Updates</h4>
+                <p class="bl_align_center">Priority support and regular feature and fix updates.</p>
             </div>
         </div>
         <!-- Dashboard Tile -->
         <div class="bl_tile bl_tile_3">
-            <div class="bl_tile_header">
-                <span>★★ Loving Block Lab? ★★</span>
-            </div>
-            <div class="bl_tile_body">
+        <div class="bl_tile_body">
+                <h4>★★ Loving Block Lab? ★★</h4>
                 <p>If Block Lab has helped you make the most of Gutenberg and custom blocks for your site, leave us a review on WordPress.org.</p>
             </div>
             <div class="bl_tile_footer">
-                <a class="bl_button" href="">★ Leave Review</a>
+                <a class="bl_button" target="_blank" href="https://wordpress.org/plugins/block-lab/#reviews">★ Leave Review</a>
             </div>
         </div>
         <!-- Dashboard Tile -->
         <div class="bl_tile bl_tile_3">
-            <div class="bl_tile_header">
-                <span>Get more out of Block Lab</span>
-            </div>
-            <div class="bl_tile_body">
+        <div class="bl_tile_body">
+                <h4>Get more out of Block Lab</h4>
                 <p>Subscribe to our newsletter for news, updates, and tutorials on working with Gutenberg.</p>
             </div>
             <div class="bl_tile_footer">
-                <a class="bl_button" href="">Subscribe</a>
+                <a class="bl_button" target="_blank" href="http://eepurl.com/dO6l8n">Subscribe</a>
             </div>
         </div>
         <div class="bl_section_heading">
