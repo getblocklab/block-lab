02bbc4ce2ae0ab831d1125976a045a5f1c0d4a19
Luke Carbis <luke.carbis@xwp.co>
2019-02-22 12:23:17 +1000

    PHPCS fixes for the upgrade view

--- php/views/upgrade.php
+++ php/views/upgrade.php
@@ -9,87 +9,87 @@
 
 ?>
 <div class="bl_admin">
-    <section class="bl_container">
-        <div class="bl_dashboard_welcome bl_tile" style="background-image: url('<?php echo esc_attr( block_lab()->get_assets_url( 'images/Block-Lab-Pro-Hero-Background-1.svg' ) ); ?>');">
-            <div class="bl_tile_body">
-                <div>
-                    <span>Block Lab Pro</span>
-                    <h1>Early Access Pass</h1>
-                    <p>Lock in a lifetime discount and help kickstart Block Lab Pro.</p>
-                    <a target="_blank" class="bl_button bl_button--white" href="https://getblocklab.com/pro">Check out Early Access</a>
-                    <input class="bl_input_text" placeholder="License key" type="text">
-                    <a class="bl_button bl_button--white-stroke" href="">Enter Your License Key</a>
-                </div>
-                <div>
-                    <img class="bl_early_access_pass" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_early_access_pass.png' ) ); ?>" alt="">
-                </div>
-            </div>
-        </div>
-        <!-- Dashboard Tile -->
-        <div class="bl_tile bl_tile_2">
-            <div class="bl_tile_header">
-                <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_newfields.svg' ) ); ?>" alt="">
-            </div>
-            <div class="bl_tile_body"> 
-                <h4 class="bl_align_center">Pro Fields</h4>
-                <p class="bl_align_center">More fields including repeater, post object, and more to help you build the custom blocks you need for yourself and your clients.</p>
-            </div>
-        </div>
-        <!-- Dashboard Tile -->
-        <div class="bl_tile bl_tile_2">
-            <div class="bl_tile_header">
-                <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_features.svg' ) ); ?>" alt="">
-            </div>
-            <div class="bl_tile_body">
-                <h4 class="bl_align_center">Pro Features</h4>
-                <p class="bl_align_center">Features including conditional logic, custom validation, and white-labeling, to help you extend Block Lab and leverage the best of Gutenberg.</p>
-            </div>
-        </div>
-        <!-- Dashboard Tile -->
-        <div class="bl_tile bl_tile_2">
-            <div class="bl_tile_header">
-            <img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_wpadmin_icon_support.svg' ) ); ?>" alt="">
-            </div>
-            <div class="bl_tile_body">
-                <h4 class="bl_align_center">Support & Updates</h4>
-                <p class="bl_align_center">Priority support and regular feature and fix updates.</p>
-            </div>
-        </div>
-        <!-- Dashboard Tile -->
-        <div class="bl_tile bl_tile_3">
-        <div class="bl_tile_body">
-                <h4>★★ Loving Block Lab? ★★</h4>
-                <p>If Block Lab has helped you make the most of Gutenberg and custom blocks for your site, leave us a review on WordPress.org.</p>
-                <a class="bl_button" target="_blank" href="https://wordpress.org/plugins/block-lab/#reviews">★ Leave Review ★</a>
-            </div>
-        </div>
-        <!-- Dashboard Tile -->
-        <div class="bl_tile bl_tile_3">
-        <div class="bl_tile_body">
-                <h4>Get more out of Block Lab</h4>
-                <p>Subscribe to our newsletter for news, updates, and tutorials on working with Gutenberg.</p>
-            </div>
-            <div class="bl_tile_footer bl_tile_footer_email">
-                <div id="mc_embed_signup">
-                    <form action="https://getblocklab.us19.list-manage.com/subscribe/post?u=f8e0c6b0ab32fc57ded52ab4a&amp;id=f05b221414" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                        <div id="mc_embed_signup_scroll">
-                        
-                    <div class="mc-field-group">
-                        <label class="bl_input_label" for="mce-EMAIL">Email Address </label>
-                        <input class="bl_input" type="email" value="" placeholder="Email Address" name="EMAIL" class="required email" id="mce-EMAIL">
-                    </div>
-                        <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f8e0c6b0ab32fc57ded52ab4a_f05b221414" tabindex="-1" value=""></div>
-                        <div class="clear"><input class="bl_button" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                        </div>
-                    </form>
-                </div>
+	<section class="bl_container">
+		<div class="bl_dashboard_welcome bl_tile" style="background-image: url('<?php echo esc_attr( block_lab()->get_assets_url( 'images/Block-Lab-Pro-Hero-Background-1.svg' ) ); ?>');">
+			<div class="bl_tile_body">
+				<div>
+					<span><?php esc_html_e( 'Block Lab Pro', 'block-lab' ); ?></span>
+					<h1><?php esc_html_e( 'Early Access Pass', 'block-lab' ); ?></h1>
+					<p><?php esc_html_e( 'Lock in a lifetime discount and help kickstart Block Lab Pro.', 'block-lab' ); ?></p>
+					<a target="_blank" class="bl_button bl_button--white" href="https://getblocklab.com/pro"><?php esc_html_e( 'Check out Early Access', 'block-lab' ); ?></a>
+					<input class="bl_input_text" placeholder="License key" type="text" />
+					<a class="bl_button bl_button--white-stroke" href=""><?php esc_html_e( 'Enter Your License Key', 'block-lab' ); ?></a>
+				</div>
+				<div>
+					<img class="bl_early_access_pass" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_early_access_pass.png' ) ); ?>" alt="" />
+				</div>
+			</div>
+		</div>
+		<!-- Dashboard Tile -->
+		<div class="bl_tile bl_tile_2">
+			<div class="bl_tile_header">
+				<img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_admin_icon_new_fields.svg' ) ); ?>" alt="" />
+			</div>
+			<div class="bl_tile_body">
+				<h4 class="bl_align_center"><?php esc_html_e( 'Pro Fields', 'block-lab' ); ?></h4>
+				<p class="bl_align_center"><?php esc_html_e( 'More fields including repeater, post object, and more to help you build the custom blocks you need for yourself and your clients.', 'block-lab' ); ?></p>
+			</div>
+		</div>
+		<!-- Dashboard Tile -->
+		<div class="bl_tile bl_tile_2">
+			<div class="bl_tile_header">
+				<img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_admin_icon_features.svg' ) ); ?>" alt="" />
+			</div>
+			<div class="bl_tile_body">
+				<h4 class="bl_align_center"><?php esc_html_e( 'Pro Features', 'block-lab' ); ?></h4>
+				<p class="bl_align_center"><?php esc_html_e( 'Features including conditional logic, custom validation, and white-labeling, to help you extend Block Lab and leverage the best of Gutenberg.', 'block-lab' ); ?></p>
+			</div>
+		</div>
+		<!-- Dashboard Tile -->
+		<div class="bl_tile bl_tile_2">
+			<div class="bl_tile_header">
+			<img class="bl_tile_icon" src="<?php echo esc_attr( block_lab()->get_assets_url( 'images/block_lab_admin_icon_support.svg' ) ); ?>" alt="" />
+			</div>
+			<div class="bl_tile_body">
+				<h4 class="bl_align_center"><?php esc_html_e( 'Support & Updates', 'block-lab' ); ?></h4>
+				<p class="bl_align_center"><?php esc_html_e( 'Priority support and regular feature and fix updates.', 'block-lab' ); ?></p>
+			</div>
+		</div>
+		<!-- Dashboard Tile -->
+		<div class="bl_tile bl_tile_3">
+		<div class="bl_tile_body">
+				<h4><?php esc_html_e( '★★ Loving Block Lab? ★★', 'block-lab' ); ?></h4>
+				<p><?php esc_html_e( 'If Block Lab has helped you build amazing custom blocks for your site, leave us a review on WordPress.org.', 'block-lab' ); ?></p>
+				<a class="bl_button" target="_blank" href="https://wordpress.org/plugins/block-lab/#reviews"><?php esc_html_e( '★ Leave Review ★', 'block-lab' ); ?></a>
+			</div>
+		</div>
+		<!-- Dashboard Tile -->
+		<div class="bl_tile bl_tile_3">
+		<div class="bl_tile_body">
+				<h4><?php esc_html_e( 'Get more out of Block Lab', 'block-lab' ); ?></h4>
+				<p><?php esc_html_e( 'Subscribe to our newsletter for news, updates, and tutorials on working with Gutenberg.', 'block-lab' ); ?></p>
+			</div>
+			<div class="bl_tile_footer bl_tile_footer_email">
+				<div id="mc_embed_signup">
+					<form action="https://getblocklab.us19.list-manage.com/subscribe/post?u=f8e0c6b0ab32fc57ded52ab4a&amp;id=f05b221414" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+							<div class="mc-field-group">
+								<label class="bl_input_label" for="mce-EMAIL">Email Address </label>
+								<input class="bl_input required email" type="email" value="" placeholder="Email Address" name="EMAIL" id="mce-EMAIL" />
+							</div>
+							<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>
+							<div class="clear">
+								<input class="bl_button button" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+							</div>
+						</div>
+					</form>
+				</div>
 
-                <!--End mc_embed_signup-->
-            </div>
-        </div>
-    </section>
+				<!--End mc_embed_signup-->
+			</div>
+		</div>
+	</section>
 </div>
