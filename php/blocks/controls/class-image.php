f4d33c03c29838416c938f58acf405890c834f1f
Ryan Kienstra <kienstraryan@gmail.com>
2019-03-25 13:01:12 -0600

    Use a Core function to get the attachment's ID
    Instead of the custom method,
    use the core function attachment_url_to_postid()
    If it returns 0, return false from validate().

--- php/blocks/controls/class-image.php
+++ php/blocks/controls/class-image.php
@@ -66,59 +66,11 @@ class Image extends Control_Abstract {
 	 * @return mixed $value The value to be made available or echoed on the front-end template.
 	 */
 	public function validate( $value, $echo ) {
-		$is_image = wp_attachment_is( 'image', $value ) && is_numeric( $value );
-
 		if ( $echo ) {
-			return $is_image ? wp_get_attachment_image_url( $value ) : '';
+			return $value;
 		} else {
-			return $is_image ? $this->get_attachment_id_from_url( $value ) : false;
+			$id = attachment_url_to_postid( $value );
+			return $id ? $id : false;
 		}
-	}
-
-	/**
-	 * Gets the attachment (image) ID, given its URL.
-	 *
-	 * Based on Micah Wood's solution,
-	 * this creates a \WP_Query to search for the attachment ID.
-	 *
-	 * @see https://wpscholar.com/blog/get-attachment-id-from-wp-image-url/
-	 * @param string $url The URL of the attachment (image).
-	 * @return int|false The attachment ID, or false if none was found.
-	 */
-	public function get_attachment_id_from_url( $url ) {
-		$image_file = basename( $url );
-		$query      = new \WP_Query(
-			array(
-				'post_type'   => 'attachment',
-				'post_status' => 'inherit',
-				'fields'      => 'ids',
-				'meta_query'  => array( // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
-					array(
-						'value'   => $image_file,
-						'compare' => 'LIKE',
-						'key'     => '_wp_attachment_metadata',
-					),
-				),
-			)
-		);
-
-		if ( ! $query->have_posts() ) {
-			return false;
-		}
-
-		foreach ( $query->posts as $post_id ) {
-			$meta = wp_get_attachment_metadata( $post_id );
-			if ( ! isset( $meta['file'], $meta['sizes'] ) ) {
-				continue;
-			}
-
-			$original_file       = basename( $meta['file'] );
-			$cropped_image_files = wp_list_pluck( $meta['sizes'], 'file' );
-			if ( $original_file === $image_file || in_array( $image_file, $cropped_image_files, true ) ) {
-				return $post_id;
-			}
-		}
-
-		return false;
 	}
 }
