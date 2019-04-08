16e99be836fa960f7284ba41cb39b84529dc1e74
Luke Carbis <luke@carb.is>
2019-04-08 22:45:37 +1000

    Add placeholder to image dropzone and tidy up styles

--- js/blocks/controls/image.js
+++ js/blocks/controls/image.js
@@ -1,11 +1,11 @@
-const { BaseControl, Button, DropZone, FormFileUpload, Spinner } = wp.components;
+const { BaseControl, Button, Placeholder, DropZone, FormFileUpload, Spinner } = wp.components;
 const { withState } = wp.compose;
 const { withSelect } = wp.data;
 const { mediaUpload, MediaUpload, MediaUploadCheck } = wp.editor;
 const { Fragment } = wp.element;
 const { __ } = wp.i18n;
 
-const ALLOWED_TYPES = [ 'image' ];
+const ALLOWED_TYPES  = [ 'image' ];
 const DEFAULT_IMG_ID = 0;
 
 const BlockLabImageControl = ( props, field, block ) => {
@@ -82,76 +82,74 @@ const BlockLabImageControl = ( props, field, block ) => {
 		}
 
 		return (
-			<BaseControl className="block-lab-media-controls" label={ field.label } help={ field.help }>
-				<Fragment>
-					{ ! isUploading && imageSrc && (
-						<img className="bl-image__img" src={ imageSrc } alt={ imageAlt } />
-					) }
-					{ ! imageSrc && (
-						<div className="bl-image__placeholder">
-							<span class="dashicons dashicons-format-image" />
-							<MediaUploadCheck>
-								<DropZone
-									onFilesDrop={ ( files ) => {
-										if ( files.length ) {
-											uploadStart();
-											uploadFiles( files );
-										}
-									} }
-								></DropZone>
-							</MediaUploadCheck>
-						</div>
-					) }
-				</Fragment>
-				<MediaUploadCheck>
-					{ isUploading && (
-						<Spinner />
-					) }
-					<div className="bl-image__buttons">
-						<FormFileUpload
-							isLarge
-							disabled={!!isUploading}
-							onChange={(event) => {
-								let files = event.target.files;
-								uploadStart(files[0].name);
-								uploadFiles( files );
-							}}
-							accept='image/*'
-							multiple={ false }
-						>
-							{ __( 'Upload', 'block-lab' ) }
-						</FormFileUpload>
-						<MediaUpload
-							gallery={ false }
-							multiple={ false }
-							onSelect={ onSelect }
-							allowedTypes={ ALLOWED_TYPES }
-							value={ attr[ field.name ] }
-							render={ ( { open } ) => (
-								<div className='components-media-library-button'>
-									<Button
+			<BaseControl className="block-lab-media-controls" label={ field.label }>
+				{ !! field.help && <p className="components-base-control__help">{ field.help }</p> }
+				{ ! isUploading && imageSrc && (
+					<img className="bl-image__img" src={ imageSrc } alt={ imageAlt } />
+				) }
+				{ ! imageSrc && (
+					<Placeholder className="bl-image__placeholder" icon="format-image" label={ __( 'Image', 'block-lab') } instructions={ __( 'Drag an image, upload a new one or select a file from your library.', 'block-lab' ) }>
+						<MediaUploadCheck>
+							<DropZone
+								onFilesDrop={ ( files ) => {
+									if ( files.length ) {
+										uploadStart();
+										uploadFiles( files );
+									}
+								} }
+							></DropZone>
+							{ isUploading && (
+								<Spinner />
+							) }
+							{ ! isUploading && (
+								<Fragment>
+									<FormFileUpload
 										isLarge
 										disabled={!!isUploading}
-										className="editor-media-placeholder__button"
-										onClick={ open }
+										onChange={(event) => {
+											let files = event.target.files;
+											uploadStart(files[0].name);
+											uploadFiles( files );
+										}}
+										accept='image/*'
+										multiple={ false }
 									>
-										{ __( 'Media Library', 'block-lab' ) }
-									</Button>
-								</div>
-							) }
-						/>
-						{ imageSrc && (
-							<Button
-								isLarge
-								disabled={!!isUploading}
-								className="bl-image__remove"
-								onClick={removeImage}
-							>
-								{ __( 'Remove', 'block-lab' ) }
-							</Button>
-						) }
-					</div>
-				</MediaUploadCheck>
+										{ __( 'Upload', 'block-lab' ) }
+									</FormFileUpload>
+									<MediaUpload
+										gallery={ false }
+										multiple={ false }
+										onSelect={ onSelect }
+										allowedTypes={ ALLOWED_TYPES }
+										value={ attr[ field.name ] }
+										render={ ( { open } ) => (
+											<div className='components-media-library-button'>
+												<Button
+													isLarge
+													disabled={!!isUploading}
+													className="editor-media-placeholder__button"
+													onClick={ open }
+												>
+													{ __( 'Media Library', 'block-lab' ) }
+												</Button>
+											</div>
+										) }
+									/>
+								</Fragment>
+								) }
+						</MediaUploadCheck>
+					</Placeholder>
+				) }
+				{ imageSrc && (
+					<Button
+						isLarge
+						disabled={!!isUploading}
+						className="bl-image__remove"
+						onClick={removeImage}
+					>
+						{ __( 'Remove', 'block-lab' ) }
+					</Button>
+				) }
 			</BaseControl>
 		);
 	} ) );
