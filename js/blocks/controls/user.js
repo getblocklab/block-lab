a4fae882bc4728645eb3ae067817cfc86a524e48
Ryan Kienstra <kienstraryan@gmail.com>
2019-04-23 03:06:17 -0500

    Update the User control to work like the Post control
    This will break backwards-compatibility.
    Maybe in the meantime I'll think about a way
    to avoid this break,
    but so far I can't see a way to avoid it.

--- js/blocks/controls/user.js
+++ js/blocks/controls/user.js
@@ -3,18 +3,34 @@ import FetchInput from './fetch-input';
 const BlockLabUserControl = ( props, field, block ) => {
 	const { setAttributes } = props;
 	const attr = { ...props.attributes };
-	const getValueFromAPI = apiResponse => apiResponse.slug ? apiResponse.slug : apiResponse;
+	const DEFAULT_ID = 0;
+	const getIdFromAPI = apiResponse => ( apiResponse && apiResponse.id ) ? apiResponse.id : DEFAULT_ID;
+	const getNameFromAPI = apiResponse => ( apiResponse && apiResponse.name ) ? apiResponse.name : '';
+
+	attr[ field.name ] = Object.assign( { id: DEFAULT_ID, userName: '' }, attr[ field.name ] );
+	const userAttribute = attr[ field.name ];
 
 	return (
 		<FetchInput
 			field={field}
 			placeholder={field.placeholder}
 			apiSlug="users"
-			value={attr[ field.name ]}
-			getValueFromAPI={getValueFromAPI}
+			value={userAttribute['id']}
+			displayValue={userAttribute['userName']}
+			getValueFromAPI={getIdFromAPI}
+			getDisplayValueFromAPI={getNameFromAPI}
 			onChange={value => {
-				attr[ field.name ] = getValueFromAPI( value )
-				setAttributes( attr )
+				if ( 'string' === typeof value ) {
+					// The value is probably from the user typing into the <input>.
+					userAttribute['userName'] = value;
+					userAttribute['id'] = DEFAULT_ID;
+				} else {
+					// The value is probably an Object, from the user selecting a link in the Popover.
+					userAttribute['userName'] = getNameFromAPI( value );
+					userAttribute['id'] = getIdFromAPI( value );
+				}
+				attr[ field.name ] = userAttribute;
+				setAttributes( attr );
 			}}
 		/>
 	);
