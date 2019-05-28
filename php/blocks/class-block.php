70f66d641037c826463ea8e95d690d5ed8244349
Ryan Kienstra <kienstraryan@gmail.com>
2019-05-28 00:46:48 -0500

    Add the location in the control class, not by default

--- php/blocks/class-block.php
+++ php/blocks/class-block.php
@@ -112,7 +112,7 @@ class Block {
 
 		if ( isset( $config['fields'] ) ) {
 			foreach ( $config['fields'] as $field ) {
-				$field_defaults = array( 'name', 'label', 'control', 'type', 'location', 'order' );
+				$field_defaults = array( 'name', 'label', 'control', 'type', 'order' );
 				$field_settings = array_diff( array_keys( $field ), $field_defaults );
 				foreach ( $field_settings as $setting ) {
 					$field['settings'][ $setting ] = $field[ $setting ];
@@ -136,12 +136,11 @@ class Block {
 
 		$config['fields'] = array();
 		foreach ( $this->fields as $key => $field ) {
-			$config['fields'][ $key ]['name']     = $field->name;
-			$config['fields'][ $key ]['label']    = $field->label;
-			$config['fields'][ $key ]['control']  = $field->control;
-			$config['fields'][ $key ]['type']     = $field->type;
-			$config['fields'][ $key ]['location'] = $field->location;
-			$config['fields'][ $key ]['order']    = $field->order;
+			$config['fields'][ $key ]['name']    = $field->name;
+			$config['fields'][ $key ]['label']   = $field->label;
+			$config['fields'][ $key ]['control'] = $field->control;
+			$config['fields'][ $key ]['type']    = $field->type;
+			$config['fields'][ $key ]['order']   = $field->order;
 
 			foreach ( $field->settings as $setting => $value ) {
 				$config['fields'][ $key ][ $setting ] = $value;
