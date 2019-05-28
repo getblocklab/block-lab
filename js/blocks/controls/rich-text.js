6ec3976b607a73d8c3bd85fbead45934a38dfe8f
Ryan Kienstra <kienstraryan@gmail.com>
2019-05-28 05:25:01 -0500

    Add left, center, and right alignment controls to Rich Text
    There are still issues to consider:
    * Deselect any other alignment control when one
    is selected
    * When aligning right, nothing can be to the left.

--- js/blocks/controls/rich-text.js
+++ js/blocks/controls/rich-text.js
@@ -1,59 +1,144 @@
 const { BaseControl } = wp.components;
 const { RichText, RichTextToolbarButton } = wp.editor;
-const { Fragment } = wp.element;
+const { registerFormatType, removeFormat, toggleFormat } = wp.richText;
 const { __ } = wp.i18n;
 const leftAlignmentControl = 'block-lab/left-alignment';
 const centerAlignmentControl = 'block-lab/center-alignment';
 const rightAlignmentControl = 'block-lab/right-alignment';
 const formattingControls = [ leftAlignmentControl, centerAlignmentControl, rightAlignmentControl, 'bold', 'italic', 'strikethrough', 'link' ];
 
+registerFormatType(
+	leftAlignmentControl,
+	{
+		title: __( 'Align Left', 'block-lab' ),
+		tagName: 'p',
+		className: 'bl-align-left',
+		attributes: {
+			align: 'style',
+		},
+		edit: ( props ) => {
+			return (
+				<RichTextToolbarButton
+					title={ __( 'Align Left', 'block-lab' ) }
+					name={ leftAlignmentControl }
+					icon="editor-alignleft"
+					isActive={ props.isActive }
+					onClick={ () => {
+						props.onChange(
+							toggleFormat(
+								props.value,
+								{
+									type: leftAlignmentControl,
+									attributes: {
+										align: 'text-align: left;',
+									}
+								},
+							)
+						);
+					} }
+				/>
+			);
+		}
+	}
+);
+
+registerFormatType(
+	centerAlignmentControl,
+	{
+		title: __( 'Align Center', 'block-lab' ),
+		tagName: 'p',
+		className: 'bl-align-center',
+		attributes: {
+			align: 'style',
+		},
+		edit: ( props ) => {
+			return (
+				<RichTextToolbarButton
+					title={ __( 'Align Center', 'block-lab' ) }
+					name={ centerAlignmentControl }
+					icon="editor-aligncenter"
+					isActive={ props.isActive }
+					onClick={ () => {
+						props.onChange(
+							toggleFormat(
+								props.value,
+								{
+									type: centerAlignmentControl,
+									attributes: {
+										align: 'text-align: center;',
+									}
+								},
+							)
+						);
+					} }
+				/>
+			);
+		}
+	}
+);
+
+registerFormatType(
+	rightAlignmentControl,
+	{
+		title: __( 'Align Right', 'block-lab' ),
+		tagName: 'p',
+		className: 'bl-align-right',
+		attributes: {
+			align: 'style',
+		},
+		edit: ( props ) => {
+			return (
+				<RichTextToolbarButton
+					title={ __( 'Align Right', 'block-lab' ) }
+					name={ rightAlignmentControl }
+					icon="editor-alignright"
+					isActive={ props.isActive }
+					onClick={ () => {
+						props.onChange(
+							toggleFormat(
+								props.value,
+								{
+									type: rightAlignmentControl,
+									attributes: {
+										align: 'text-align: right;',
+									}
+								},
+							)
+						);
+					} }
+				/>
+			);
+		}
+	}
+);
+
 const BlockLabRichTextControl = ( props, field, block ) => {
 	const { setAttributes } = props
 	const attr = { ...props.attributes }
 
 	return (
-		<Fragment>
-			<RichTextToolbarButton
-				title={ __( 'Align Left', 'block-lab' ) }
-				name={ leftAlignmentControl }
-				icon="editor-alignleft"
-				isActive={ false }
+		<BaseControl label={field.label} className="block-lab-rich-text-control " help={field.help}>
+			{
+			/*
+			* @todo: Resolve known issue with toolbar not disappearing on blur
+			* @see: https://github.com/WordPress/gutenberg/issues/7463
+			*/
+			}
+			<RichText
+				placeholder={field.placeholder || ''}
+				keepPlaceholderOnFocus={true}
+				defaultValue={field.default}
+				value={attr[ field.name ]}
+				className='input-control'
+				multiline={!!field.multiline}
+				inlineToolbar={true}
+				formattingControls={formattingControls}
+				onChange={richTextControl => {
+					attr[field.name] = richTextControl
+					setAttributes(attr)
+				}}
 			/>
-			<RichTextToolbarButton
-				title={ __( 'Align Center', 'block-lab' ) }
-				name={ centerAlignmentControl }
-				icon="editor-aligncenter"
-				isActive={ false }
-			/>
-			<RichTextToolbarButton
-				title={ __( 'Align Right', 'block-lab' ) }
-				name={ rightAlignmentControl }
-				icon="editor-alignright"
-				isActive={ false }
-			/>
-			<BaseControl label={field.label} className="block-lab-rich-text-control " help={field.help}>
-				{
-				/*
-				* @todo: Resolve known issue with toolbar not disappearing on blur
-				* @see: https://github.com/WordPress/gutenberg/issues/7463
-				*/
-				}
-				<RichText
-					placeholder={field.placeholder || ''}
-					keepPlaceholderOnFocus={true}
-					defaultValue={field.default}
-					value={attr[ field.name ]}
-					className='input-control'
-					multiline={!!field.multiline}
-					inlineToolbar={true}
-					formattingControls={formattingControls}
-					onChange={richTextControl => {
-						attr[ field.name ] = richTextControl
-						setAttributes( attr )
-					}}
-				/>
-			</BaseControl>
-		</Fragment>
+		</BaseControl>
 	)
 }
 
