6002e8116b7b39845f42b0abdd6cd03b61e6818d
Ryan Kienstra <kienstraryan@gmail.com>
2019-06-20 13:45:59 +0200

    Create a trait to more easily test Controls
    This uses an array of arrays,
    in order to test that the Control's settings are correct.

---
+++ tests/php/blocks/controls/trait-control-helper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Trait with a helper method for testing controls.
+ *
+ * @package Block_Lab
+ */
+
+/**
+ * Trait with a helper method.
+ */
+trait Control_Helper {
+
+	/**
+	 * Assert that the settings are correct.
+	 *
+	 * @param array $expected_settings The expected settings, an array of arrays.
+	 * @param array $actual_settings The actual settings, an array of Control_Setting instances.
+	 */
+	public function assert_correct_settings( $expected_settings, $actual_settings ) {
+		foreach ( $actual_settings as $settings_index => $setting ) {
+			$expected_setting = $expected_settings[ $settings_index ];
+			foreach ( $setting as $setting_key => $setting_value ) {
+				unset( $setting_value );
+				$this->assertEquals( $expected_setting[ $setting_key ],  $setting->$setting_key );
+			}
+		}
+
+		$this->assertEquals( count( $expected_settings ), count( $actual_settings ) );
+	}
+}
