434e62e19ede3b4f7ffe8a1f2e7139c5792aeb7a
Luke Carbis <luke@carb.is>
2019-07-11 08:17:01 +1000

    Add underscore to filter name

--- php/blocks/class-loader.php
+++ php/blocks/class-loader.php
@@ -187,7 +187,7 @@ class Loader extends Component_Abstract {
 			}
 		}
 
-		return apply_filters( 'blocklab_get_block_attributes', $attributes );
+		return apply_filters( 'block_lab_get_block_attributes', $attributes );
 	}
 
 	/**
