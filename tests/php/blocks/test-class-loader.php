e72f6cc955b724a3668ee40a7ef9c9e9d841f7e7
Luke Carbis <luke.carbis@xwp.co>
2019-07-22 20:03:42 +1000

    Add unit tests for enqueuing global styles

--- tests/php/blocks/test-class-loader.php
+++ tests/php/blocks/test-class-loader.php
@@ -47,34 +47,39 @@ class Test_Loader extends \WP_UnitTestCase {
 		$block->from_array( array( 'name' => $block_name ) );
 
 		// Test that the do_action() call with this action runs, and that it allows enqueuing a script.
-		add_action( 'block_lab_render_template', function( $block ) use ( $block_name, $slug, $script_url) {
-			if ( $block_name === $block->name ) {
-				wp_enqueue_script( $slug, $script_url, array(), '0.1', true );
+		add_action(
+			'block_lab_render_template',
+			function( $block ) use ( $block_name, $slug, $script_url ) {
+				if ( $block_name === $block->name ) {
+					wp_enqueue_script( $slug, $script_url, array(), '0.1', true );
+				}
 			}
-		} );
+		);
 
 		$this->instance->render_block_template( $block, array() );
 		$scripts = wp_scripts();
 		$script  = $scripts->registered[ $slug ];
 
-		$this->assertTrue( in_array( $slug, $scripts->queue ) );
+		$this->assertTrue( in_array( $slug, $scripts->queue, true ) );
 		$this->assertEquals( $slug, $script->handle );
 		$this->assertEquals( $script_url, $script->src );
-
 
 		// Test that the do_action() call with the dynamic name runs, like 'block_lab_render_template_bl-dynamic-testing-slug'.
 		$slug       = 'bl-dynamic-testing-slug';
 		$script_url = 'https://example.com/another-script.js';
 
-		add_action( "block_lab_render_template_{$block_name}", function( $block ) use ( $block_name, $slug, $script_url) {
-			wp_enqueue_script( $slug, $script_url, array(), '0.1', true );
-		} );
+		add_action(
+			"block_lab_render_template_{$block_name}",
+			function( $block ) use ( $block_name, $slug, $script_url ) {
+				wp_enqueue_script( $slug, $script_url, array(), '0.1', true );
+			}
+		);
 
 		$this->instance->render_block_template( $block, array() );
 		$scripts = wp_scripts();
 		$script  = $scripts->registered[ $slug ];
 
-		$this->assertTrue( in_array( $slug, $scripts->queue ) );
+		$this->assertTrue( in_array( $slug, $scripts->queue, true ) );
 		$this->assertEquals( $slug, $script->handle );
 		$this->assertEquals( $script_url, $script->src );
 	}
@@ -130,5 +135,49 @@ class Test_Loader extends \WP_UnitTestCase {
 		$this->instance->enqueue_block_styles( 'does-not-exist', 'block' );
 		$this->assertNotContains( $block_handle, $wp_styles->queue );
 		$this->assertArrayNotHasKey( $block_handle, $wp_styles->registered );
+	}
+
+	/**
+	 * Test enqueue_global_styles.
+	 *
+	 * @covers \Block_Lab\Blocks\Loader::enqueue_global_styles()
+	 */
+	public function test_enqueue_global_styles() {
+		$wp_styles       = wp_styles();
+		$enqueue_handle  = 'block-lab__global-styles';
+		$stylesheet_path = get_template_directory();
+
+		if ( ! file_exists( $stylesheet_path . '/blocks/' ) ) {
+			mkdir( $stylesheet_path . '/blocks/' );
+			mkdir( $stylesheet_path . '/blocks/css/' );
+		}
+
+		// In order of reverse priority.
+		$files = array(
+			"{$stylesheet_path}/blocks/blocks.css",
+			"{$stylesheet_path}/blocks/css/blocks.css",
+		);
+
+		// Remove previous template files so that we can correctly check load order.
+		foreach ( $files as $file ) {
+			if ( file_exists( $file ) ) {
+				unlink( $file );
+			}
+		}
+
+		// Check that the correct stylesheet is enqueued.
+		foreach ( $files as $key => $file ) {
+			file_put_contents( $file, '' ); // @codingStandardsIgnoreLine
+			$file_url = str_replace( untrailingslashit( ABSPATH ), '', $file );
+
+			$this->instance->enqueue_global_styles();
+
+			$this->assertContains( $enqueue_handle, $wp_styles->queue );
+			$this->assertArrayHasKey( $enqueue_handle, $wp_styles->registered );
+			$this->assertSame( $wp_styles->registered[ $enqueue_handle ]->src, $file_url, "Trying to enqueue file #{$key} ({$file_url})." );
+
+			wp_deregister_style( $enqueue_handle );
+			wp_dequeue_style( $enqueue_handle );
+		}
 	}
 }
