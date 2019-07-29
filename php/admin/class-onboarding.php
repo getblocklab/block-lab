a9fdb9b643bd4ad07dee10275e23d447eee71746
Luke Carbis <luke@carb.is>
2019-07-29 21:18:15 +1000

    Only delete the example post id option once the post is published

--- php/admin/class-onboarding.php
+++ php/admin/class-onboarding.php
@@ -69,7 +69,9 @@ class Onboarding extends Component_Abstract {
 			 * After we've shown the Add to Post message once, we can delete the option. This will
 			 * ensure that no further onboarding messages are shown.
 			 */
-			delete_option( $this->option );
+			if ( 'publish' === get_post_status( $example_post_id ) ) {
+				delete_option( $this->option );
+			}
 		}
 
 		if ( 'draft' !== get_post_status( $example_post_id ) ) {
