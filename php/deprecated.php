df797fd7bc41556ab47a7119d1db0f2d343e7615
Luke Carbis <luke@carb.is>
2019-08-13 15:05:47 +1000

    Handle deprecated methods in the component abstract

--- php/deprecated.php
+++ php/deprecated.php
@@ -2,6 +2,10 @@
 /**
  * Deprecated functions.
  *
+ * Deprecated methods can also appear as functions here, with the format namespace__class__method().
+ *
+ * @see Block_Lab\Component_Abstract->_call()
+ *
  * @package   Block_Lab
  * @copyright Copyright(c) 2018, Block Lab
  * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
@@ -17,10 +21,10 @@
 function block_lab_deprecated_function( $function, $version, $replacement ) {
 	if ( WP_DEBUG && apply_filters( 'deprecated_function_trigger_error', true ) ) {
 		trigger_error( // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-			wp_kses_post(
+			esc_html(
 				sprintf(
 					// translators: (1) a function name, (2) a version number, (3) a function name.
-					__( '%1$s is <strong>deprecated</strong> since Block Lab version %2$s! Use %3$s instead.', 'block-lab' ),
+					__( '%1$s is deprecated since Block Lab version %2$s! Use %3$s instead.', 'block-lab' ),
 					$function,
 					$version,
 					$replacement
