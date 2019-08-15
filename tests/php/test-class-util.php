fdb9a17954d1366962a5e458ca362e0cc28bc597
Ryan Kienstra <kienstraryan@gmail.com>
2019-08-14 21:40:18 -0500

    Add unit tests for get_icons() and allowed_svg_tags()
    This is more to prevent regressions,
    as these are straight-forward functions.

--- tests/php/test-class-util.php
+++ tests/php/test-class-util.php
@@ -25,6 +25,18 @@ class Test_Util extends Abstract_Template {
 	}
 
 	/**
+	 * Teardown.
+	 *
+	 * @inheritdoc
+	 */
+	public function tearDown() {
+		remove_all_filters( 'block_lab_template_path' );
+		remove_all_filters( 'block_lab_icons' );
+		remove_all_filters( 'block_lab_allowed_svg_tags' );
+		parent::tearDown();
+	}
+
+	/**
 	 * Test is_pro.
 	 *
 	 * This method is normally accessed via a Plugin::__call(), as a magic method.
@@ -162,5 +174,84 @@ class Test_Util extends Abstract_Template {
 
 		// Similarly, the filter above should cause this to include the template in the alternate path, among other templates.
 		$this->assertTrue( in_array( $full_alternate_block_path, $this->instance->locate_template( $templates, '', false ), true ) );
+	}
+
+	/**
+	 * Test get_icons.
+	 *
+	 * @covers \Block_Lab\Util::get_icons()
+	 */
+	public function test_get_icons() {
+		$icons = $this->instance->get_icons();
+		$this->assertEquals(
+			'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M6.5 10h-2v7h2v-7zm6 0h-2v7h2v-7zm8.5 9H2v2h19v-2zm-2.5-9h-2v7h2v-7zm-7-6.74L16.71 6H6.29l5.21-2.74m0-2.26L2 6v2h19V6l-9.5-5z"/></svg>',
+			$icons['account_balance']
+		);
+		$this->assertEquals(
+			'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18 9v4H6V9H4v6h16V9h-2z"/></svg>',
+			$icons['space_bar']
+		);
+
+		$additional_icon = '<svg></svg>';
+		$icon_name       = 'additional_icon';
+
+		add_filter(
+			'block_lab_icons',
+			function( $icons ) use( $additional_icon, $icon_name ) {
+				$icons[ $icon_name ] = $additional_icon;
+				return $icons;
+			}
+		);
+
+		// The filter should add the additional icon.
+		$icons = $this->instance->get_icons();
+		$this->assertEquals( $additional_icon, $icons[ $icon_name ] );
+	}
+
+	/**
+	 * Test allowed_svg_tags.
+	 *
+	 * @covers \Block_Lab\Util::allowed_svg_tags()
+	 */
+	public function test_allowed_svg_tags() {
+		$this->assertEquals(
+			array(
+				'svg'    => array(
+					'xmlns'   => true,
+					'width'   => true,
+					'height'  => true,
+					'viewbox' => true,
+				),
+				'g'      => array( 'fill' => true ),
+				'title'  => array( 'title' => true ),
+				'path'   => array(
+					'd'       => true,
+					'fill'    => true,
+					'opacity' => true,
+				),
+				'circle' => array(
+					'cx'   => true,
+					'cy'   => true,
+					'r'    => true,
+					'fill' => true,
+				),
+			),
+			$this->instance->allowed_svg_tags()
+		);
+
+		$additional_tag_name       = 'example';
+		$additional_tag_attributes = array( 'bax' => true );
+
+		add_filter(
+			'block_lab_allowed_svg_tags',
+			function( $allowed_tags ) use ( $additional_tag_name, $additional_tag_attributes ) {
+				$allowed_tags[ $additional_tag_name ] = $additional_tag_attributes;
+				return $allowed_tags;
+			}
+		);
+
+		// The filter should add the additional tag and attributes.
+		$svg_tags = $this->instance->allowed_svg_tags();
+		$this->assertEquals( $additional_tag_attributes, $svg_tags[ $additional_tag_name ] );
 	}
 }
