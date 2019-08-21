6c991b07e6e938fd95e4794ae2319e013b69ce14
Luke Carbis <luke@carb.is>
2019-08-21 15:44:48 +1000

    Fix broken removeRow method

--- js/blocks/components/repeater-rows.js
+++ js/blocks/components/repeater-rows.js
@@ -39,17 +39,17 @@ import { Fields } from './';
 	 * Sub-fields in the Repeater control have parents.
 	 * This looks for a parent in each field, and returns a parent as long as they don't have different parents.
 	 *
-	 * @param {Object} fields The fields in which to look for the parent.
+	 * @param {Object} subFields The fields in which to look for the parent.
 	 * @return {String|null} parent The parent of the fields.
 	 */
-	getParent( fields ) {
+	getParent( subFields ) {
 		let parent = null;
-		for ( const field in fields ) {
-			if ( fields.hasOwnProperty( field ) ) {
-				if ( parent && parent !== fields[ field ].parent ) {
+		for ( const field in subFields ) {
+			if ( subFields.hasOwnProperty( field ) ) {
+				if ( parent && parent !== subFields[ field ].parent ) {
 					return null;
 				}
-				parent = fields[ field ].parent;
+				parent = subFields[ field ].parent;
 			}
 		}
 
@@ -65,8 +65,9 @@ import { Fields } from './';
 		return () => {
 			const { parentBlockProps } = this.props;
 			const attr = { ...parentBlockProps.attributes };
-			const parentName = this.getParent( this.props.fields );
+			const parentName = this.getParent( this.props.subFields );
 			const repeaterRows = attr[ parentName ];
+
 			if ( ! repeaterRows ) {
 				return;
 			}
