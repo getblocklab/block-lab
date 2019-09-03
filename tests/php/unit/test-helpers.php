6714a2281a8a98791bfacbfb27f36a05e3d61d5a
Ryan Kienstra <kienstraryan@gmail.com>
2019-09-03 18:24:44 -0500

    Update the filter name in the unit tests
    I had forgotten to do this in a few places,
    including the tearDown() method.

--- tests/php/unit/test-helpers.php
+++ tests/php/unit/test-helpers.php
@@ -18,7 +18,7 @@ class Test_Helpers extends \WP_UnitTestCase {
 	 * @inheritdoc
 	 */
 	public function tearDown() {
-		remove_all_filters( 'is_block_field_name_allowed' );
+		remove_all_filters( 'block_lab_default_fields' );
 		$GLOBALS['block_lab_attributes'] = array();
 		$GLOBALS['block_lab_config']     = array();
 		parent::tearDown();
@@ -91,8 +91,9 @@ class Test_Helpers extends \WP_UnitTestCase {
 		$this->assertEmpty( $return_value );
 		$this->assertEmpty( $echoed_value );
 
+		$default_fields_filter = 'block_lab_default_fields';
 		add_filter(
-			'block_lab_default_fields',
+			$default_fields_filter,
 			function( $default_fields ) use ( $additional_field_name ) {
 				$default_fields[] = $additional_field_name;
 			}
@@ -105,10 +106,10 @@ class Test_Helpers extends \WP_UnitTestCase {
 		// In case the filter accidentally doesn't return anything, there should still not be a fatal error, there should just be no output.
 		$this->assertEmpty( $return_value );
 		$this->assertEmpty( $echoed_value );
-		remove_all_filters( 'is_block_field_name_allowed' );
+		remove_all_filters( $default_fields_filter );
 
 		add_filter(
-			'block_lab_default_fields',
+			$default_fields_filter,
 			function( $default_fields ) use ( $additional_field_name ) {
 				$default_fields[] = $additional_field_name;
 				return $default_fields;
