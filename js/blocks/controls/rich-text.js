45323d21ecf6b2143e770f03174df7f26850da63
Ryan Kienstra <kienstraryan@gmail.com>
2019-09-18 02:07:24 -0500

    Begin adding list controls to the Rich Text control
    Still, it might not be possible to get
    this to work,
    as the buttons appear in the popover.

--- js/blocks/controls/rich-text.js
+++ js/blocks/controls/rich-text.js
@@ -1,5 +1,118 @@
+/**
+ * WordPress dependencies
+ */
 const { BaseControl } = wp.components;
 const { RichText } = wp.blockEditor;
+const { RichTextToolbarButton } = wp.blockEditor;
+const { Fragment } = wp.element;
+const { __ } = wp.i18n;
+const {
+	__unstableChangeListType: changeListType,
+	__unstableIsListRootSelected: isListRootSelected,
+	__unstableIsActiveListType: isActiveListType,
+	applyFormat,
+	registerFormatType,
+	removeFormat,
+} = wp.richText;
+
+const listControlName = 'block-lab/rich-text-list-controls';
+const tagName = 'ul';
+
+/**
+ * Gets the styling for a given alignment.
+ *
+ * @param {string} alignment The alignment, like 'left' or 'right'.
+ * @return {string} The alignment style, like text-align: left;.
+ */
+const getListStyle = ( alignment ) => {
+	return `text-align: ${ alignment }`;
+};
+
+/**
+ * The list buttons to set the <ul> and <li> format.
+ *
+ * @param {Object} props The component properties.
+ * @param {Object} props.value The value.
+ * @param {Function} props.onChange The handler for changes of the toolbar.
+ */
+const ListToolbar = ( { value, onChange } ) => {
+	const setAttributes = () => {};
+
+	return (
+		<Fragment>
+			<RichTextToolbarButton
+				icon="editor-ul"
+				title={ __( 'Convert to unordered list', 'block-lab' ) }
+				isActive={ !! value && !! value.name && isActiveListType( value, 'ul', tagName ) }
+				onClick={ () => {
+					onChange( changeListType( value, { type: 'ul' } ) );
+
+					if ( isListRootSelected( value ) ) {
+						setAttributes( { ordered: false } );
+					}
+				} }
+			/>
+			<RichTextToolbarButton
+				icon="editor-ol"
+				title={ __( 'Convert to ordered list', 'block-lab' ) }
+				isActive={ !! value && !! value.name && isActiveListType( value, 'ol', tagName ) }
+				onClick={ () => {
+					onChange( changeListType( value, { type: 'ol' } ) );
+
+					if ( isListRootSelected( value ) ) {
+						setAttributes( { ordered: true } );
+					}
+				} }
+			/>
+		</Fragment>
+	);
+};
+
+registerFormatType(
+	listControlName,
+	{
+		title: __( 'List Type Controls', 'block-lab' ),
+		tagName,
+		className: 'bl-aligned',
+		attributes: {
+			align: 'style',
+		},
+		edit: ( { onChange, value } ) => {
+			return (
+				<ListToolbar
+					value={ value }
+					onChange={ ( newListType ) => {
+						if ( newListType ) {
+							onChange(
+								applyFormat(
+									value,
+									{
+										type: listControlName,
+										attributes: {
+											align: getListStyle( newListType ),
+										},
+									},
+								)
+							);
+						} else {
+							onChange(
+								removeFormat(
+									value,
+									{
+										type: listControlName,
+										attributes: {
+											align: getListStyle( value ),
+										},
+									},
+								)
+							);
+						}
+					} }
+				/>
+			);
+		},
+	}
+);
 
 const BlockLabRichTextControl = ( props ) => {
 	const { field, getValue, instanceId, onChange } = props;
@@ -27,7 +140,8 @@ const BlockLabRichTextControl = ( props ) => {
 				multiline={ true }
 				inlineToolbar={ true }
 				onChange={ onChange }
-			/>
+			>
+			</RichText>
 		</BaseControl>
 	);
 };
