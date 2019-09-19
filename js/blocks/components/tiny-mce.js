56d9649998e7d45b21ff21c91f0d619e32e6be6c
Ryan Kienstra <kienstraryan@gmail.com>
2019-09-19 00:24:33 -0500

    Add a strikethrough button for backwards compatibility
    There was one in the previous RichText,
    though it looks like there isn't simply
    a button for code, like there was in the
    previous <RichText>.

--- js/blocks/components/tiny-mce.js
+++ js/blocks/components/tiny-mce.js
@@ -75,7 +75,7 @@ class TinyMCE extends Component {
 				content_css: false,
 				fixed_toolbar_container: `#toolbar-${ editorId }`,
 				setup: this.onSetup,
-				toolbar1: 'bold,italic,bullist,numlist,outdent,indent,alignleft,aligncenter,alignright,link,unlink,wp_add_media',
+				toolbar1: 'bold,italic,bullist,numlist,outdent,indent,alignleft,aligncenter,alignright,link,unlink,wp_add_media,strikethrough',
 				toolbar2: '',
 			},
 		} );
