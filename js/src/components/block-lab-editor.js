558fdf706673dd05d422236b9b398c95572f7fa0
Ryan Kienstra <kienstraryan@gmail.com>
2019-11-23 20:09:57 -0600

    Port PHP rendering to React, mainly copying
    Port some of the PHP from render_fields_meta_box()
    into the React component.
    Still, it doesn't actuall add fields,
    and doesn't save anything.

--- js/src/components/block-lab-editor.js
+++ js/src/components/block-lab-editor.js
@@ -2,11 +2,11 @@
  * WordPress dependencies
  */
 const { blocks } = wp;
-const { TextControl } = wp.components;
+const { Button } = wp.components;
 const { compose } = wp.compose;
 const { withDispatch, withSelect } = wp.data;
 const { Component } = wp.element;
-const { __ } = wp.i18n;
+const { __, sprintf } = wp.i18n;
 
 /**
  * The Block Lab field editor.
@@ -18,14 +18,46 @@ class BlockLabEditor extends Component {
 	 * @return {Function} The rendered component.
 	 */
 	render() {
-		const { content, onChange } = this.props;
-
 		return (
-			<TextControl
-				label={ __( 'Proof of concept that it is possible to directly change post content from a meta box', 'block-lab' ) }
-				value={ content }
-				onChange={ onChange }
-			/>
+			<div>
+				<div className="block-fields-list">
+					<table className="widefat">
+						<thead>
+							<tr>
+								<th className="block-fields-sort"></th>
+								<th className="block-fields-label">
+									{ __( 'Field Label', 'block-lab' ) }
+								</th>
+								<th className="block-fields-name">
+									{ __( 'Field Name', 'block-lab' ) }
+								</th>
+								<th className="block-fields-control">
+									{ __( 'Field Type', 'block-lab' ) }
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td colSpan="4">
+									<div className="block-fields-rows">
+										<p className="block-no-fields">
+											{ sprintf( __( 'Click %sAdd Field%s below to add your first field.', 'block-lab' ), '<strong>', '</strong' ) }
+										</p>
+										{ /* @todo: implement render_fields_meta_box_row() here. */ }
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className="block-fields-actions-add-field">
+					<Button type="button" aria-label="Add Field" className="block-fields-action" id="block-add-field">
+						<span className="dashicons dashicons-plus"></span>
+						{ __( 'Add Field', 'block-lab' ) }
+					</Button>
+					{ /* @todo: reimplement render_fields_meta_box_row and render_fields_sub_rows() */ }
+				</div>
+			</div>
 		);
 	}
 }
