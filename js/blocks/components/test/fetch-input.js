c5561e9205b765dfffd38d0735970418cf0e451b
Ryan Kienstra <kienstraryan@gmail.com>
2020-04-29 18:26:50 -0500

    Update npm packages, to be in sync with e2e test
    So these tests will pass if/when that PR is merged.
    Also, remove some needless React imports.

--- js/blocks/components/test/fetch-input.js
+++ js/blocks/components/test/fetch-input.js
@@ -2,8 +2,7 @@
  * External dependencies
  */
 import '@testing-library/jest-dom/extend-expect';
-import React from 'react';
-import { fireEvent, render, screen, waitForDomChange } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 /**
  * WordPress dependencies
@@ -22,10 +21,8 @@ const value = 'this is a value';
 const displayValue = 'example-display-value';
 const mockOnChange = jest.fn();
 const setup = ( props ) => {
-	const utils = render(
-		<FetchInput { ...props } />
-	);
-	const input = utils.getByLabelText( label );
+	const utils = render( <FetchInput { ...props } /> );
+	const input = utils.getAllByLabelText( label )[ 0 ];
 	return {
 		input,
 		...utils,
@@ -42,39 +39,56 @@ const baseProps = {
 
 describe( 'FetchInput', () => {
 	it( 'displays the value if there is no displayValue', () => {
-		const { input } = setup( { value, field: { label }, onChange: mockOnChange } );
+		const { input } = setup( {
+			value,
+			field: { label },
+			onChange: mockOnChange,
+		} );
 		expect( input.value ).toBe( value );
 	} );
 
 	it( 'displays the displayValue instead of the value if both are present', () => {
-		const { input } = setup( { value, displayValue, field: { label }, onChange: mockOnChange } );
+		const { input } = setup( {
+			value,
+			displayValue,
+			field: { label },
+			onChange: mockOnChange,
+		} );
 		expect( input.value ).toBe( displayValue );
 	} );
 
-	it( 'displays the popover when there are search results to show', async () => {
+	it( 'displays the popover when there are search results to show', () => {
 		const exampleResult = 'this-is-a-result';
 		const results = [ exampleResult ];
-		apiFetch.mockImplementationOnce( () => new Promise( ( resolve ) => resolve( results ) ) );
+		apiFetch.mockImplementationOnce(
+			() => new Promise( ( resolve ) => resolve( results ) )
+		);
 		const { input } = setup( baseProps );
 		fireEvent.focus( input );
 
-		const suggestion = await screen.findByText( exampleResult );
-		expect( suggestion ).not.toBe( null );
+		waitFor( () =>
+			expect( screen.getByText( exampleResult ) ).not.toBe( null )
+		);
 	} );
 
 	it.each( [
 		[ [], true ],
 		[ [ 'a-result' ], false ],
 		[ [ 'first-result', 'another-result' ], false ],
-	] )( 'should only have the error class if there are no results after focusing',
-		async ( apiResults, expected ) => {
-			apiFetch.mockImplementationOnce( () => new Promise( ( resolve ) => resolve( apiResults ) ) );
+	] )(
+		'should only have the error class if there are no results after focusing',
+		( apiResults, expected ) => {
+			apiFetch.mockImplementationOnce(
+				() => new Promise( ( resolve ) => resolve( apiResults ) )
+			);
 			const { input } = setup( baseProps );
 			fireEvent.focus( input );
 
-			await waitForDomChange( { container: input } ).then( () => {
-				expect( input.classList.contains( 'text-control__error' ) ).toStrictEqual( expected );
-			} );
+			waitFor( () =>
+				expect(
+					input.classList.contains( 'text-control__error' )
+				).toStrictEqual( expected )
+			);
 		}
 	);
 } );
--- js/blocks/components/test/fields.js
+++ js/blocks/components/test/fields.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import '@testing-library/jest-dom/extend-expect';
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 /**
