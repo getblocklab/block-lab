7f0276fcfbe6612a03c5b3b368044b77b26d1cee
Ryan Kienstra <kienstraryan@gmail.com>
2020-05-02 22:53:45 -0500

    Revert needless changes to test/fetch-input.js
    These were from linting,
    but they pass linting using lint:js

--- js/blocks/components/test/fetch-input.js
+++ js/blocks/components/test/fetch-input.js
@@ -39,30 +39,19 @@ const baseProps = {
 
 describe( 'FetchInput', () => {
 	it( 'displays the value if there is no displayValue', () => {
-		const { input } = setup( {
-			value,
-			field: { label },
-			onChange: mockOnChange,
-		} );
+		const { input } = setup( { value, field: { label }, onChange: mockOnChange } );
 		expect( input.value ).toBe( value );
 	} );
 
 	it( 'displays the displayValue instead of the value if both are present', () => {
-		const { input } = setup( {
-			value,
-			displayValue,
-			field: { label },
-			onChange: mockOnChange,
-		} );
+		const { input } = setup( { value, displayValue, field: { label }, onChange: mockOnChange } );
 		expect( input.value ).toBe( displayValue );
 	} );
 
 	it( 'displays the popover when there are search results to show', () => {
 		const exampleResult = 'this-is-a-result';
 		const results = [ exampleResult ];
-		apiFetch.mockImplementationOnce(
-			() => new Promise( ( resolve ) => resolve( results ) )
-		);
+		apiFetch.mockImplementationOnce( () => new Promise( ( resolve ) => resolve( results ) ) );
 		const { input } = setup( baseProps );
 		fireEvent.focus( input );
 
@@ -75,12 +64,9 @@ describe( 'FetchInput', () => {
 		[ [], true ],
 		[ [ 'a-result' ], false ],
 		[ [ 'first-result', 'another-result' ], false ],
-	] )(
-		'should only have the error class if there are no results after focusing',
+	] )( 'should only have the error class if there are no results after focusing',
 		( apiResults, expected ) => {
-			apiFetch.mockImplementationOnce(
-				() => new Promise( ( resolve ) => resolve( apiResults ) )
-			);
+			apiFetch.mockImplementationOnce( () => new Promise( ( resolve ) => resolve( apiResults ) ) );
 			const { input } = setup( baseProps );
 			fireEvent.focus( input );
 
