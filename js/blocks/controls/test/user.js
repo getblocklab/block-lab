d6e9a22dfd0285320ba2760abcd37eb2b70b94bd
Ryan Kienstra <kienstraryan@gmail.com>
2020-05-07 14:44:50 -0500

    Add a test for the User control
    Very similar to the Post control,
    but the API returns slightly
    different values.

---
+++ js/blocks/controls/test/user.js
@@ -0,0 +1,63 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, getByText, render, waitFor } from '@testing-library/react';
+
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+
+/**
+ * Internal dependencies
+ */
+import BlockLabUserControl from '../user';
+
+jest.mock( '@wordpress/api-fetch' );
+
+const props = {
+	field: {
+		label: 'This is a user field',
+		help: 'This is help text for the user field',
+	},
+	getValue: jest.fn(),
+	onChange: jest.fn(),
+};
+
+describe( 'User', () => {
+	it( 'has the label', () => {
+		const { getByLabelText } = render( <BlockLabUserControl { ...props } /> );
+		expect( getByLabelText( props.field.label ) ).toBeInTheDocument();
+	} );
+
+	it( 'has the help text', () => {
+		render( <BlockLabUserControl { ...props } /> );
+		expect( getByText( document, props.field.help ) ).toBeInTheDocument();
+	} );
+
+	it( 'sends a new value to the onChange handler', async () => {
+		render( <BlockLabUserControl { ...props } /> );
+		const input = document.querySelector( 'input' );
+		const user = {
+			id: 32,
+			name: 'Jose Adams',
+		};
+
+		// Mock the API fetch function that gets the users.
+		apiFetch.mockImplementationOnce(
+			() => new Promise( ( resolve ) => resolve( [ user ] ) )
+		);
+
+		// Focus the <input>, so the popover appears with user suggestion(s).
+		fireEvent.focus( input );
+
+		// Click to select a user.
+		await waitFor( () =>
+			fireEvent.click( getByText( document, user.name ) )
+		);
+
+		// The onChange handler should be called with the selected user.
+		expect( props.onChange ).toHaveBeenCalledWith( { id: user.id, userName: user.name } );
+	} );
+} );
