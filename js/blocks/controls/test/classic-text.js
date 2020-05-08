31528416100422e1c8921730dc6a786958b471b8
Ryan Kienstra <kienstraryan@gmail.com>
2020-05-08 13:24:15 -0500

    Refactor classic text component test to use suggestions from testingjavascript.com
    Instead of nesting this in describe,
    use a single test() block.

--- js/blocks/controls/test/classic-text.js
+++ js/blocks/controls/test/classic-text.js
@@ -2,26 +2,12 @@
  * External dependencies
  */
 import '@testing-library/jest-dom/extend-expect';
-import { getByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
 import BlockLabClassicTextControl from '../classic-text';
-
-const props = {
-	field: {
-		label: 'This is an example label',
-		help: 'This is some help text',
-		default: 'This is a default value',
-	},
-	getValue: jest.fn(),
-	onChange: jest.fn(),
-	rowIndex: 1,
-	parentBlockProps: {
-		clientId: '85811934-a952-4cc1-8fca-01b825c11cfe',
-	},
-};
 
 global.wp = {
 	oldEditor: {
@@ -41,9 +27,21 @@ window.tinymce = {
 	},
 };
 
-describe( 'ClassicText', () => {
-	it( 'has the help text', () => {
-		render( <BlockLabClassicTextControl { ...props } /> );
-		expect( getByText( document, props.field.help ) ).toBeInTheDocument();
-	} );
+test( 'classic text control', () => {
+	const props = {
+		field: {
+			label: 'This is an example label',
+			help: 'This is some help text',
+			default: 'This is a default value',
+		},
+		getValue: jest.fn(),
+		onChange: jest.fn(),
+		rowIndex: 1,
+		parentBlockProps: {
+			clientId: '85811934-a952-4cc1-8fca-01b825c11cfe',
+		},
+	};
+
+	const { getByText } = render( <BlockLabClassicTextControl { ...props } /> );
+	expect( getByText( props.field.help ) ).toBeInTheDocument();
 } );
