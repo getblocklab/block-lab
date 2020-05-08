99467d97f6ea2a0b254a59bd0c29d9d802fd3673
Ryan Kienstra <kienstraryan@gmail.com>
2020-05-08 12:54:12 -0500

    Refactor the checkbox control, per testingjavascript.com suggestions
    This uses user.click() to approximate a user
    unchecking the box, instead of sending a change event.

--- js/blocks/controls/test/checkbox.js
+++ js/blocks/controls/test/checkbox.js
@@ -2,48 +2,33 @@
  * External dependencies
  */
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import user from '@testing-library/user-event';
 
 /**
  * Internal dependencies
  */
 import BlockLabCheckboxControl from '../checkbox';
 
-const field = {
-	help: 'Here is help text for the checkbox field',
-	default: '1',
-};
-const mockOnChange = jest.fn();
-const setup = () => {
-	const utils = render(
+test( 'checkbox control', () => {
+	const field = {
+		help: 'Here is help text for the checkbox field',
+		default: '1',
+	};
+	const mockOnChange = jest.fn();
+	const { getByRole, getByText } = render(
 		<BlockLabCheckboxControl
 			field={ field }
 			getValue={ jest.fn() }
 			onChange={ mockOnChange }
 		/>
 	);
-	const checkbox = document.querySelector( 'input' );
-	return {
-		checkbox,
-		...utils,
-	};
-};
+	const checkbox = getByRole( 'checkbox' );
 
-describe( 'Checkbox', () => {
-	it( 'displays the default value if there is no entered value', () => {
-		const { checkbox } = setup();
-		expect( checkbox.value ).toBe( field.default );
-	} );
+	getByText( field.help );
+	expect( checkbox ).toBeChecked( !! field.default );
 
-	it( 'has the help text', () => {
-		const { getByText } = setup();
-		expect( getByText( field.help ) ).toBeInTheDocument();
-	} );
-
-	it.each( 'sends a new value to the onChange when the checkbox is clicked', () => {
-		const newValue = '0';
-		const { checkbox } = setup();
-		fireEvent.change( checkbox, { target: { value: newValue } } );
-		expect( mockOnChange ).toHaveBeenCalledWith( newValue );
-	} );
+	// Click the bock to uncheck it, and verify that false is sent to the onChange handler.
+	user.click( checkbox );
+	expect( mockOnChange ).toHaveBeenCalledWith( false );
 } );
