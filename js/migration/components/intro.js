4d70930370ec300d6d42e4bf3bb1237d08729ca1
Ryan Kienstra <kienstraryan@gmail.com>
2021-03-25 09:28:11 -0600

    Change the migration page copy to mention new editor
    Thanks to Rob for his copy,
    this was copied verbatim (at least should have been).

--- js/migration/components/intro.js
+++ js/migration/components/intro.js
@@ -17,21 +17,15 @@ import { __ } from '@wordpress/i18n';
  */
 const Intro = () => {
 	const developerNoticeUrl = 'https://getblocklab.com/migrating-to-genesis-custom-blocks/';
-	const announcementUrl = 'https://getblocklab.com/the-block-lab-team-are-joining-wp-engine/';
 
 	return (
 		<>
 			<div>
 				<h1>{ __( 'Migrating to Genesis Custom Blocks', 'block-lab' ) }</h1>
-				<p>{ __( 'In April, the Block Lab team joined the Genesis team at WP Engine. With our full-time focus, we’re very excited about the future of custom block tooling in WordPress. You can read more about that moment in this', 'block-lab' ) } <a target="_blank" rel="noopener noreferrer" className="text-purple-600 underline hover:text-purple-700" href={ announcementUrl }>{ __( 'announcement post', 'block-lab' ) }.</a></p>
-				<p>
-					{ __( 'As part of this move, we have been working on a new plugin that is based on what we developed at Block Lab.', 'block-lab' ) }
-					&nbsp;
-					{ __( 'Genesis Custom Blocks is now the home of all our custom block efforts and what we have planned is very very cool!', 'block-lab' ) }
-					&nbsp;
-					{ __( 'Version 1.0 of this plugin is now released and has full feature parity with Block Lab.', 'block-lab' ) }
-				</p>
-				<p>{ __( 'To continue receiving the best of what our team is building, we encourage you to migrate over. Our migration tool makes this nice and easy, and for the majority of use cases, completely automated.', 'block-lab' ) }</p>
+				<p>{ __( 'We’ve been busy! Since the Block Lab team joined WP Engine in 2020, the new plugin (Genesis Custom Blocks) has got a whole lot better. Plenty of things under the hood, but the biggest change is the new Block Builder interface.', 'block-lab' ) }</p>
+				<p>{ __( 'Check it out below!', 'block-lab' ) }</p>
+				<p>{ __( 'Migrating from Block Lab to Genesis Custom Blocks is super easy, seamless, and (for 95% of users) completely automated.', 'block-lab' ) }</p>
+				<p>{ __( 'To continue receiving the best of what our team is building, we encourage you to migrate over.', 'block-lab' ) }</p>
 				<div className="dev-notice">
 					<svg fill="currentColor" viewBox="0 0 20 20">
 						<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path>
